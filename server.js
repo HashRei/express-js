e41c41652cd11c945677ac8ac4734e8eded6aa95
HashRei <hashrei@protonmail.com>
2022-06-14 21:49:50 +0200

    Add html rendering with ejs

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const { request } = require("express")
 const express = require("express")
 const app = express()
 
+app.set("view engine", "ejs")
+
 app.get('/', (request, respond) => {
     console.log("Hello")
-    respond.download('server.js')
+    respond.render("index")
 })
 
 app.listen(3000)
